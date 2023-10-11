220dc968e35c11e56f4027b017cd4882b779bc58
aivy09 <147442706+aivy09@users.noreply.github.com>
2023-10-11 14:01:15 -0700

    Added units conversion

--- src/app.js
+++ src/app.js
@@ -26,7 +26,9 @@ let windElement = document.querySelector("#wind");
 let dateElement = document.querySelector("#date");
 let iconElement = document.querySelector("#icon");
 
-temperatureElement.innerHTML= Math.round(response.data.main.temp);
+celTemp = response.data.main.temp;
+
+temperatureElement.innerHTML= Math.round(celTemp);
 cityElement.innerHTML= response.data.name;
 descriptionElement.innerHTML = response.data.weather[0].description;
 humidityElement.innerHTML = response.data.main.humidity;
@@ -48,7 +50,34 @@ function handleSubmit(event){
     search(cityInputElement.value);
 }
 
+function showFahTemp(event){
+    event.preventDefault();
+    celsiusLink.classList.remove("active");
+    fahrenheitLink.classList.add("active");
+    let temperatureElement = document.querySelector("#temperature");
+    let fahTemp = ((celTemp * 9)/5 + 32);
+    temperatureElement.innerHTML = Math.round(fahTemp);
+}
+
+function showCelTemp(event){
+    event.preventDefault();
+    celsiusLink.classList.add("active");
+        fahrenheitLink.classList.remove("active");
+
+        let temperatureElement = document.querySelector("#temperature");
+        temperatureElement.innerHTML = Math.round(celTemp);
+}
+
+let celTemp = null;
 
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
+
+let fahrenheitLink= document.querySelector("#fah-link");
+fahrenheitLink.addEventListener("click", showFahTemp);
+
+let celsiusLink= document.querySelector("#cel-link");
+celsiusLink.addEventListener("click", showCelTemp);
+
+search("Seattle");

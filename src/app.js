7fc8c23ed15c00f9560185791aa5f25f199f54a1
aivy09 <147442706+aivy09@users.noreply.github.com>
2023-10-12 15:42:08 -0700

    Removed forecast from HTML and added to JS

--- src/app.js
+++ src/app.js
@@ -15,6 +15,31 @@ if (minutes < 10) {
     return `${day}  ${hours}:${minutes}`;
 }
 
+function displayForecast(){
+    let forecastElement = document.querySelector("#forecast");
+
+let forecastHTML = `<div class="row">`;
+let days = ["Thu", "Fri", "Sat", "Sun"];
+
+days.forEach(function(day){
+forecastHTML = forecastHTML + `
+   
+        <div class = "col-2">
+          <div class="weather-forecast-date">
+          ${day}</div>
+          <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAQBJREFUaN7t2csNwyAMBmBGYYSMwhgdgxEYjRW6ARu4HNyqB0CKednElf5b2/hLSALGAICRHKMABSjgUMDdD7xfLifkxByoJOJ33O3/nwHIhVgsKDWKriXhb+0WQD6wJxZegvhlADzrcUDhpeFlpwLyAa5BZ711Na4pgAXFNxFdABw2K4r/R9iRgLiw+N89MQSATxvYFN8F2DB0qkOJCggbi/8m9AASA0AiAXBuA0ziKIDACBAogMgIECkAYBUFKEABzwOIf4yKf5HJnkqIn8wxmk775y5oxC8pj1jUH9FWEd/YOqK1eERz94j2euFqUCF7NzjYbzHpLqUCFKCAJfkAq7RimK7qUtAAAAAASUVORK5CYII=" alt="" width="40px"/>
+        <div class="weather-forecast-temperature">
+          <span class="weather-forecast-temperature-max">18º</span> 
+          <span class="weather-forecast-temperature-min">12º</span> 
+        </div>
+        </div>
+      `;
+});
+ 
+forecastHTML = forecastHTML + `</div>`;
+forecastElement.innerHTML = forecastHTML;
+
+}
 
 
 function displayTemperature(response){
@@ -25,6 +50,7 @@ let humidityElement = document.querySelector("#humidity");
 let windElement = document.querySelector("#wind");
 let dateElement = document.querySelector("#date");
 let iconElement = document.querySelector("#icon");
+
 
 celTemp = response.data.main.temp;
 
@@ -81,3 +107,4 @@ let celsiusLink= document.querySelector("#cel-link");
 celsiusLink.addEventListener("click", showCelTemp);
 
 search("Seattle");
+displayForecast();

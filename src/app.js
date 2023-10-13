68a4a48d47cf1991e7ea73d7d0df0d057f31da69
aivy09 <147442706+aivy09@users.noreply.github.com>
2023-10-12 19:18:32 -0700

    Called API in JS

--- src/app.js
+++ src/app.js
@@ -15,7 +15,8 @@ if (minutes < 10) {
     return `${day}  ${hours}:${minutes}`;
 }
 
-function displayForecast(){
+function displayForecast(response){
+    console.log(response.data.daily);
     let forecastElement = document.querySelector("#forecast");
 
 let forecastHTML = `<div class="row">`;
@@ -41,6 +42,11 @@ forecastElement.innerHTML = forecastHTML;
 
 }
 
+function getForecast(coordinates){
+    let apiKey = "f81614abe2395d5dfecd45b9298041de";
+    let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayForecast);
+}
 
 function displayTemperature(response){
 let temperatureElement = document.querySelector("#temperature");
@@ -62,6 +68,9 @@ windElement.innerHTML = Math.round(response.data.wind.speed);
 dateElement.innerHTML = formatDate(response.data.dt * 1000);
 iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`);
 iconElement.setAttribute("alt", response.data.weather[0].description);
+
+getForecast(response.data.coord);
+
 }
 
 function search(city) {
@@ -107,4 +116,3 @@ let celsiusLink= document.querySelector("#cel-link");
 celsiusLink.addEventListener("click", showCelTemp);
 
 search("Seattle");
-displayForecast();
